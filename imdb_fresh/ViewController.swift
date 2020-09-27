b566cc625c70970040a4deac52ccab9fac658973
Roman Svintitskyy <romansvintitskyy2023@u.northwestern.edu>
2020-09-26 23:05:16 -0500

    Initial Commit

---
+++ imdb_fresh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imdb_fresh
+//
+//  Created by Roman Svintitskyy on 26.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
