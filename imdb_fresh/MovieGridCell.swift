dd79b9f4adcc42dca5d2b6ad3dcd9dd3e949b4a2
Roman Svintitskyy <romansvintitskyy2023@u.northwestern.edu>
2020-10-02 21:40:59 -0500

    week 2

---
+++ imdb_fresh/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  imdb_fresh
+//
+//  Created by Roman Svintitskyy on 02.10.2020.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
